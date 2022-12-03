5b127fc2a0bfe0ffbd4bc7d24c77cbf9be042639
Jose roberto Vargas Arango <joserobertovargasarango@MacBook-Pro-de-Jose.local>
2022-12-03 17:49:06 -0500

    agregue las funcionalidades de la nav, y el titulo principal

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,46 @@ const items = [
       quantity: 20
     }
   ]
+
+  const body = document.body
+  const btnTheme  = document.getElementById('theme-btn') 
+
+  const cartBtnOpen = document.getElementById('cart-btn')
+  const cartBtnClose = document.getElementById('close-cart')
+  const cartContainer  =document.getElementById('cart-container')
+
+  const menuBtn = document.getElementById('menu-btn')
+  const menuBtnClose = document.getElementById('close-menu')
+  const menuContainer  =document.getElementById('menu-container')
+
+
+
+
+  const darkThemChange = () =>{
+   
+      if(btnTheme.classList.contains('bx-sun')){
+        btnTheme.classList.replace('bx-sun', 'bx-moon')
+
+      }else{
+        btnTheme.classList.replace('bx-moon', 'bx-sun')
+        
+      }
+      body.classList.toggle('dark')
+         
+  }
+  btnTheme.addEventListener('click', e => darkThemChange())
+
+
+// funcionalidad boton-cart
+  cartBtnOpen.addEventListener('click', e => cartContainer.classList.remove('hiden')) 
+
+  cartBtnClose.addEventListener('click', e => cartContainer.classList.add('hiden'))
+
+// funcionalidad boton menu
+menuBtn .addEventListener('click', e => menuContainer.classList.remove('hiden')) 
+
+menuBtnClose.addEventListener('click', e => menuContainer .classList.add('hiden'))
+
+
+
+  
