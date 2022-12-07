21f10004efdf04420e39a480ee5e72cd16c15c25
camunozn <camunozn89@gmail.com>
2022-12-06 22:17:03 -0500

    commit inicial rama

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,107 +1,106 @@
 const items = [
-    {
-      id: 1,
-      name: 'Hoodies',
-      price: 14.00,
-      image: 'assets/images/featured1.png',
-      category: 'hoodies',
-      quantity: 10
-    },
-    {
-      id: 2,
-      name: 'Shirts',
-      price: 24.00,
-      image: 'assets/images/featured2.png',
-      category: 'shirts',
-      quantity: 15
-    },
-    {
-      id: 3,
-      name: 'Sweatshirts',
-      price: 24.00,
-      image: 'assets/images/featured3.png',
-      category: 'shirts',
-      quantity: 20
-    }
-  ]
+  {
+    id: 1,
+    name: 'Hoodies',
+    price: 14.0,
+    image: 'assets/images/featured1.png',
+    category: 'hoodies',
+    quantity: 10,
+  },
+  {
+    id: 2,
+    name: 'Shirts',
+    price: 24.0,
+    image: 'assets/images/featured2.png',
+    category: 'shirts',
+    quantity: 15,
+  },
+  {
+    id: 3,
+    name: 'Sweatshirts',
+    price: 24.0,
+    image: 'assets/images/featured3.png',
+    category: 'shirts',
+    quantity: 20,
+  },
+];
 
-  // declaracion de variables
+///////////////////////////////////////////////////////////
+// declaracion de variables
+const body = document.body;
+const btnTheme = document.getElementById('theme-btn');
 
+const cartBtnOpen = document.getElementById('cart-btn');
+const cartBtnClose = document.getElementById('close-cart');
+const cartContainer = document.getElementById('cart-container');
 
-  const body = document.body
-  const btnTheme  = document.getElementById('theme-btn') 
+const menuBtn = document.getElementById('menu-btn');
+const menuBtnClose = document.getElementById('close-menu');
+const menuContainer = document.getElementById('menu-container');
 
-  const cartBtnOpen = document.getElementById('cart-btn')
-  const cartBtnClose = document.getElementById('close-cart')
-  const cartContainer  =document.getElementById('cart-container')
+const btnMenuHome = document.getElementById('menu-home');
+const btnMenuProducts = document.getElementById('menu-products');
 
-  const menuBtn = document.getElementById('menu-btn')
-  const menuBtnClose = document.getElementById('close-menu')
-  const menuContainer  =document.getElementById('menu-container')
+const navBarScroller = document.getElementById('navBar');
 
-  const navBarScroller = document.getElementById('navBar');
+const btnAddProducts = document.querySelectorAll('btn-add-producs');
 
-
-  const btnMenuHome = document.getElementById('menu-home')
-  const btnMenuProducts = document.getElementById('menu-products')
-
-
-  btnMenuHome .addEventListener('click', e => menuContainer.classList.add('hiden')) 
-  btnMenuProducts .addEventListener('click', e => menuContainer.classList.add('hiden')) 
-
-
-
-
-  
-
-
-// funcion botones de cerrar y abrir
-
-  const darkThemChange = () =>{
-   
-      if(btnTheme.classList.contains('bx-moon')){
-        btnTheme.classList.replace('bx-moon', 'bx-sun')
-
-      }else{
-        btnTheme.classList.replace('bx-sun', 'bx-moon')
-        
-      }
-      body.classList.toggle('dark')
-         
+///////////////////////////////////////////////////////////
+// funcion para pasar a modo oscuro y vice versa
+const darkThemChange = () => {
+  if (btnTheme.classList.contains('bx-moon')) {
+    btnTheme.classList.replace('bx-moon', 'bx-sun');
+  } else {
+    btnTheme.classList.replace('bx-sun', 'bx-moon');
   }
-  btnTheme.addEventListener('click', e => darkThemChange())
+  body.classList.toggle('dark');
+};
+btnTheme.addEventListener('click', e => darkThemChange());
 
+///////////////////////////////////////////////////////////
+// funcionalidad abrir y cerrar carrito
+cartBtnOpen.addEventListener('click', e =>
+  cartContainer.classList.remove('hiden')
+);
 
-// funcionalidad boton-cart
+cartBtnClose.addEventListener('click', e =>
+  cartContainer.classList.add('hiden')
+);
 
-  cartBtnOpen.addEventListener('click', e => cartContainer.classList.remove('hiden')) 
+///////////////////////////////////////////////////////////
+// funcionalidad abrir y cerrar menu
+menuBtn.addEventListener('click', e => menuContainer.classList.remove('hiden'));
 
-  cartBtnClose.addEventListener('click', e => cartContainer.classList.add('hiden'))
+menuBtnClose.addEventListener('click', e =>
+  menuContainer.classList.add('hiden')
+);
 
-// funcionalidad boton menu
+///////////////////////////////////////////////////////////
+// funcionalidad cerrar menu al dar click en un enlace
+btnMenuHome.addEventListener('click', e =>
+  menuContainer.classList.add('hiden')
+);
+btnMenuProducts.addEventListener('click', e =>
+  menuContainer.classList.add('hiden')
+);
 
-menuBtn .addEventListener('click', e => menuContainer.classList.remove('hiden')) 
-
-menuBtnClose.addEventListener('click', e => menuContainer.classList.add('hiden'))
-
-
-// agregando color a la barra de navegacion sise detecta un scroll
-
-
+///////////////////////////////////////////////////////////
+// funcion para dar color a la barra de navegacion si se detecta un scroll
 let lastscroll = 0;
- window.addEventListener('scroll', () =>{
-  const  currentScroll = window.pageYOffset
+const colorbarraLateralScroll = () => {
+  const currentScroll = window.pageYOffset;
   lastscroll = currentScroll;
-  if(lastscroll <= 10 ){
-    navBarScroller.classList.remove('scroller-nav')
-
-  }else{
-    navBarScroller.classList.add('scroller-nav')
-
+  if (lastscroll <= 10) {
+    navBarScroller.classList.remove('scroller-nav');
+  } else {
+    navBarScroller.classList.add('scroller-nav');
   }
-  
- })
+};
 
- const colorbarraLateralScroll = () =>{
-  
- }
+window.addEventListener('scroll', () => colorbarraLateralScroll());
+
+///////////////////////////////////////////////////////////
+// funcionalidad llenar carrito
+btnAddProducts.forEach(el => {
+  el.addEventListener();
+});
